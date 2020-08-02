d45ea1854e48f394adf188648918c1c54d3eed4c
mikemalkhasyan <mkrtich.malkhasyan1996@gmail.com>
2020-08-02 20:40:03 +0400

    - Client App - Initial commit

--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -1,33 +1,21 @@
 import React, {useState} from 'react'
 import gql from 'graphql-tag'
-import PetBox from '../components/PetBox'
-import NewPet from '../components/NewPet'
 import { useQuery, useMutation } from '@apollo/react-hooks'
+import PetsList from '../components/PetsList'
+import NewPetModal from '../components/NewPetModal'
 import Loader from '../components/Loader'
+
 
 export default function Pets () {
   const [modal, setModal] = useState(false)
-  
+
+
   const onSubmit = input => {
     setModal(false)
   }
-
-  const petsList = pets.data.pets.map(pet => (
-    <div className="col-xs-12 col-md-4 col" key={pet.id}>
-      <div className="box">
-        <PetBox pet={pet} />
-      </div>
-    </div>
-  ))
   
   if (modal) {
-    return (
-      <div className="row center-xs">
-        <div className="col-xs-8">
-          <NewPet onSubmit={onSubmit} onCancel={() => setModal(false)}/>
-        </div>
-      </div>
-    )
+    return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />
   }
 
   return (
@@ -44,9 +32,7 @@ export default function Pets () {
         </div>
       </section>
       <section>
-        <div className="row">
-          {petsList}
-        </div>
+        <PetsList />
       </section>
     </div>
   )
