ba16f32b048a36fc05302924de1e893621edb167
mikemalkhasyan <mkrtich.malkhasyan1996@gmail.com>
2020-08-02 15:59:19 +0400

    - Query Types & Resolvers

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -5,19 +5,21 @@
 
 module.exports = {
   Query: {
-    
-  },
-  Mutation: {
-    
-  },
-  Pet: {
-    img(pet) {
-      return pet.type === 'DOG'
-        ? 'https://placedog.net/300/300'
-        : 'http://placekitten.com/300/300'
+    pets(_, __, ctx) {
+      return ctx.models.Pet.findMany();
     }
   },
-  User: {
-    
-  }
+  // Mutation: {
+  //
+  // },
+  // Pet: {
+  //   img(pet) {
+  //     return pet.type === 'DOG'
+  //       ? 'https://placedog.net/300/300'
+  //       : 'http://placekitten.com/300/300'
+  //   }
+  // },
+  // User: {
+  //
+  // }
 }
--- api/src/schema.js
+++ api/src/schema.js
@@ -4,7 +4,21 @@ const { gql } = require('apollo-server')
  * Type Definitions for our Schema using the SDL.
  */
 const typeDefs = gql`
-  
+    type User {
+      id: ID!
+      username: String!
+    }
+    
+    type Pet {
+        id: ID!
+        name: String!
+        type: String!
+        createdAt: String!
+    }
+    
+    type Query {
+        pets: [Pet]!
+    }
 
 `;
 
